693dbec0ea50384a6a2f08dd1f1bfd67e9886142
Joe Warren <joe@Joes-MBP.attlocal.net>
2017-11-25 12:30:58 -0600

    before using babel class properties transform

---
+++ src/components/Action.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Action = props => {
+  return (
+    <div>
+      <p>
+        <button onClick={props.handlePick} disabled={!props.hasOptions}>
+          What should I do?{" "}
+        </button>
+      </p>
+    </div>
+  );
+};
+
+export default Action;
---
+++ src/components/AddOption.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default class AddOption extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    };
+  }
+  handleAddOption(e) {
+    e.preventDefault(); //prevents full page refresh
+
+    const option = e.target.elements.option.value.trim(); // .trim removes whitespace
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => ({ error }));
+
+    // if no error, clear input
+    if (!error) {
+      e.target.elements.option.value = "";
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button> Add Option </button>
+        </form>
+      </div>
+    );
+  }
+}
