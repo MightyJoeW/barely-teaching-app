4972d16eac291e522c02bc53c787494e8d2abbde
Joe Warren <joe@Joes-MBP.attlocal.net>
2017-11-26 13:24:47 -0600

    add 404

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -10,21 +10,24 @@ import BarelyTeachingApp from "./components/BarelyTeachingApp";
 //ROUTES
 const LandingPage = () => <div>this is from my landing page component</div>;
 
-const Journal = () => <div>this is my journal component</div>;
+const JournalPage = () => <div>this is my journal component</div>;
 
-const KidFree = () => <div>this is my kid free component</div>;
+const KidFreePage = () => <div>this is my kid free component</div>;
 
-const Help = () => <div>this is my help component</div>;
+const HelpPage = () => <div>this is my help component</div>;
+
+const NotFoundPage = () => <div>404! Page Not Found...this feels bad.</div>;
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route exact path="/" component={LandingPage} />
       <Route path="/tasks" component={BarelyTeachingApp} />
-      <Route path="/journal" component={Journal} />
-      <Route path="/kidfree" component={KidFree} />
-      <Route path="/help" component={Help} />
-    </div>
+      <Route path="/journal" component={JournalPage} />
+      <Route path="/kidfree" component={KidFreePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
