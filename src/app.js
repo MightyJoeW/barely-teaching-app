67e02bcd58da4a6bb26279d32efa7e606236041c
Joe Warren <joe@Joes-MBP.attlocal.net>
2017-11-26 13:15:51 -0600

    create routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 //COMPONENTS
-import IndecisionApp from "./components/IndecisionApp";
+import BarelyTeachingApp from "./components/BarelyTeachingApp";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+//ROUTES
+const LandingPage = () => <div>this is from my landing page component</div>;
+
+const Journal = () => <div>this is my journal component</div>;
+
+const KidFree = () => <div>this is my kid free component</div>;
+
+const Help = () => <div>this is my help component</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/tasks" component={BarelyTeachingApp} />
+      <Route path="/journal" component={Journal} />
+      <Route path="/kidfree" component={KidFree} />
+      <Route path="/help" component={Help} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
