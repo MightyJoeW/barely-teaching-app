500ddfbe884ae85db477ae793c8ba7e6d9f883af
Joe Warren <joe@Joes-MacBook-Pro.local>
2017-12-01 14:01:26 -0600

    setup formatting for period and createdAt

--- src/views/Reports/components/ReportListItem.js
+++ src/views/Reports/components/ReportListItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
+
 const ReportListItem = ({ id, student_name, period, createdAt }) => (
   <div>
     <Link to={`/reports/edit/${id}`}>
       <h3>{student_name} </h3>
     </Link>
     <p>
-      {period} - {createdAt}
+      {numeral(period).format("0o")}
+      -
+      {moment(createdAt).format("MMMM Do, YYYY")}
     </p>
   </div>
 );
