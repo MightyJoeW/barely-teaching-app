c19c0a89cab4f205df0dc2529d1354252683cd13
Joe Warren <joe@Joes-MacBook-Pro.local>
2017-12-01 12:31:11 -0600

    setup production build and server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -9,7 +10,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-const port = 3000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}.`);
 });
