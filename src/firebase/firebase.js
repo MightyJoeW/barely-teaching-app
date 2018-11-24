edbc8695166dc447ab6d185679db2d2001d01f57
Joe Warren <joe.warren7@gmail.com>
2018-11-24 12:10:32 -0600

    fix: update firebase links

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,14 @@
 import firebase from '@firebase/app';
-import '@firebase/firestore'
+import '@firebase/firestore';
 import 'firebase/auth';
 import 'firebase/database';
 
+import { apiKey, authDomain, databaseURL } from '../../util';
+
 const config = {
-  // apiKey: process.env.FIREBASE_API_KEY,
-  apiKey: 'AIzaSyDvOMxKl4sEHS9rofJLIHQgBXB0qSEWGXI',
-  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  authDomain: 'barely-teaching-app.firebaseapp.com',
-  // databaseURL: process.env.FIREBASE_DATABASE_URL,
-  databaseURL: 'https://barely-teaching-app.firebaseio.com/',
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
@@ -21,114 +20,3 @@ const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export { firebase, googleAuthProvider, database as default };
-
-// //child_removed
-// database.ref("reports").on("child_removed", snapshot => {
-//   console.log(snapshot.key, snapshot.val());
-// });
-
-// //child_changed
-// database.ref("reports").on("child_changed", snapshot => {
-//   console.log(snapshot.key, snapshot.val());
-// });
-
-// //child_added
-// database.ref("reports").on("child_added", snapshot => {
-//   console.log(snapshot.key, snapshot.val());
-// });
-
-// // database
-// //   .ref("reports")
-// //   .once("value")
-// //   .then(snapshot => {
-// //     const reports = [];
-
-// //     snapshot.forEach(childSnapshot => {
-// //       reports.push({
-// //         id: childSnapshot.key,
-// //         ...childSnapshot.val()
-// //       });
-// //     });
-// //     console.log(reports);
-// //   });
-
-// // database.ref("reports").on("value", snapshot => {
-// //   const reports = [];
-
-// //   snapshot.forEach(childSnapshot => {
-// //     reports.push({
-// //       id: childSnapshot.key,
-// //       ...childSnapshot.val()
-// //     });
-// //   });
-// //   console.log(reports);
-// // });
-
-// database.ref("reports").push({
-//   student_name: "Joe",
-//   period: 1,
-//   createdAt: 1000,
-//   note: "Good"
-// });
-
-// // database.ref("notes/-L-KcJkET3ShzlZcfCFN").remove();
-
-// // database.ref("notes").push({
-// //   title: "Course Topics",
-// //   body: "React, Angular, Python"
-// // });
-
-// // database.ref().on("value", snapshot => {
-// //   const val = snapshot.val();
-// //   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
-// // });
-
-// // database
-// //   .ref("location/city")
-// //   .once("value")
-// //   .then(snapshot => {
-// //     const val = snapshot.val();
-// //     console.log(val);
-// //   })
-// //   .catch(e => {
-// //     console.log("Error fetching data", e);
-// //   });
-
-// // database
-// //   .ref()
-// //   .set({
-// //     name: "Joe Warren",
-// //     age: 29,
-// //     stressLevel: 6,
-// //     job: {
-// //       title: "Software developer",
-// //       company: "Nudle"
-// //     },
-// //     location: {
-// //       city: "Dallas",
-// //       country: "United States"
-// //     }
-// //   })
-// //   .then(() => {
-// //     console.log("Data is saved!");
-// //   })
-// //   .catch(e => {
-// //     console.log("This failed.", e);
-// //   });
-// // database.ref().update({
-// //   stressLevel: 9,
-// //   "job/company": "Amazon",
-// //   "location/city": "Seattle"
-// // });
-
-// // database.ref("isSingle").set(null);
-
-// // database
-// //   .ref("isSingle")
-// //   .remove()
-// //   .then(() => {
-// //     console.log("Data was removed");
-// //   })
-// //   .catch(e => {
-// //     console.log("Did not remove data", e);
-// //   });
